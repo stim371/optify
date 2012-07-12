3effc55d11715f722d53dda9a5834ce1c0defc03
Joel Stimson <stimsojr@gmail.com>
2012-07-11 21:59:40 -0700

    switched from module to class

--- lib/optify.rb
+++ lib/optify.rb
@@ -1,3 +1,30 @@
-module Optify
+require 'httparty'
+
+class Optify
+  include HTTParty
+  base_uri 'https://api.optify.net/v1'
+  
+  def initialize(access_token)
+    self.class.default_params :access_token => access_token
+  end
+  
+  def sites(options={})
+    site_path = "/sites"
+    site_path << ".json" if options[:format] == "json"
+    
+    self.class.get(site_path, :query => options).body
+  end
+  
+  def site_by_id(id,options={})
+    site_path = "/sites/#{id.to_s}"
+    site_path << ".json" if options[:format] == "json"
+    
+    self.class.get(site_path, :query => options).body
+  end
+  
+  def who_am_i
+    whois_path = "https://api.optify.net/whoami"
+    self.class.get(whois_path).body.chomp
+  end
   
 end
--- lib/optify/version.rb
+++ lib/optify/version.rb
@@ -1,3 +1,3 @@
-module Optify
+class Optify
   VERSION = "0.0.1"
 end
