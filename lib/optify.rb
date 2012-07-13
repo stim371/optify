1ac6cb18a2643f5aa17200787f52661319ccee30
Kamil Jiwa <kamil@optify.net>
2012-07-12 18:29:29 -0700

    Add visitor methods. Parse the response into a hash. Update version.

--- lib/optify.rb
+++ lib/optify.rb
@@ -1,30 +1,38 @@
 require 'httparty'
+require 'json'
 
 class Optify
   include HTTParty
-  base_uri 'https://api.optify.net/v1'
+  base_uri 'https://api.optify.net'
+  format :json
   
   def initialize(access_token)
     self.class.default_params :access_token => access_token
   end
   
   def sites(options={})
-    site_path = "/sites"
-    site_path << ".json" if options[:format] == "json"
-    
-    self.class.get(site_path, :query => options).body
+    site_path = "/v1/sites.json"
+    self.class.get(site_path, :query => options).parsed_response
   end
   
   def site_by_id(id,options={})
-    site_path = "/sites/#{id.to_s}"
-    site_path << ".json" if options[:format] == "json"
-    
-    self.class.get(site_path, :query => options).body
+    site_path = "/v1/sites.json/#{id.to_s}"
+    self.class.get(site_path, :query => options).parsed_response
+  end
+
+  def visitors_by_site_id(site_id,options={})
+    path = "/v1/sites/#{site_id.to_s}/visitors.json";
+    self.class.get(path, :query => options).parsed_response
+  end
+
+  def visitor_by_id(site_id,visitor_id,options = {})
+    path = "/v1/sites/#{site_id.to_s}/visitors/#{visitor_id.to_s}.json"
+    self.class.get(path, :query => options).parsed_response
   end
   
   def who_am_i
-    whois_path = "https://api.optify.net/whoami"
-    self.class.get(whois_path).body.chomp
+    whois_path = "/whoami"
+    self.class.get(whois_path, :format => 'text/plain').parsed_response.chomp
   end
   
 end
