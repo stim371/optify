32ab91e58fd511a66b3cb668d5a3b9c696e61170
Kamil Jiwa <kamil@optify.net>
2012-07-12 18:49:28 -0700

    Convert date parameters into the expected format

--- lib/optify.rb
+++ lib/optify.rb
@@ -1,10 +1,11 @@
 require 'httparty'
-require 'json'
 
 class Optify
   include HTTParty
   base_uri 'https://api.optify.net'
   format :json
+
+  @@date_format = '%Y-%m-%d %H:%m:%S'
   
   def initialize(access_token)
     self.class.default_params :access_token => access_token
@@ -21,11 +22,23 @@ class Optify
   end
 
   def visitors_by_site_id(site_id,options={})
+    options.keys.each do |key|
+      if options[key].kind_of? Date
+        options[key] = options[key].strftime(@@date_format)
+      end
+    end
+
     path = "/v1/sites/#{site_id.to_s}/visitors.json";
     self.class.get(path, :query => options).parsed_response
   end
 
   def visitor_by_id(site_id,visitor_id,options = {})
+    options.keys.each do |key|
+      if options[key].kind_of? Date
+        options[key] = options[key].strftime(@@date_format)
+      end
+    end
+
     path = "/v1/sites/#{site_id.to_s}/visitors/#{visitor_id.to_s}.json"
     self.class.get(path, :query => options).parsed_response
   end
