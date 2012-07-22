521bd5ac66689bb872c35878cbac3d1e64496bb5
Joel Stimson <stimsojr@gmail.com>
2012-07-22 02:46:55 -0700

    added rake tasks for guard and removing fixtures

--- Rakefile
+++ Rakefile
@@ -7,3 +7,13 @@ desc "Run all the rspec examples"
 task :spec do
   system "bundle exec rspec -c spec"
 end
+
+desc "Start guard"
+task :guard do
+  system "bundle exec guard"
+end
+
+desc "Delete fixtures to run tests with new data"
+task :cleanup do
+  system "rm -rf spec/fixtures"
+end
--- lib/optify.rb
+++ lib/optify.rb
@@ -4,8 +4,6 @@ class Optify
   include HTTParty
   base_uri 'https://api.optify.net'
   format :json
-
-  @@date_format = '%Y-%m-%d %H:%m:%S'
   
   def initialize(access_token)
     self.class.default_params :access_token => access_token
@@ -13,34 +11,22 @@ class Optify
   
   def sites(options={})
     site_path = "/v1/sites.json"
-    self.class.get(site_path, :query => options).parsed_response
+    self.class.get(site_path, :query => clean(options)).parsed_response
   end
   
   def site_by_id(id,options={})
     site_path = "/v1/sites.json/#{id.to_s}"
-    self.class.get(site_path, :query => options).parsed_response
+    self.class.get(site_path, :query => clean(options)).parsed_response
   end
 
   def visitors_by_site_id(site_id,options={})
-    options.keys.each do |key|
-      if options[key].kind_of? Date
-        options[key] = options[key].strftime(@@date_format)
-      end
-    end
-
     path = "/v1/sites/#{site_id.to_s}/visitors.json";
-    self.class.get(path, :query => options).parsed_response
+    self.class.get(path, :query => clean(options)).parsed_response
   end
 
   def visitor_by_id(site_id,visitor_id,options = {})
-    options.keys.each do |key|
-      if options[key].kind_of? Date
-        options[key] = options[key].strftime(@@date_format)
-      end
-    end
-
     path = "/v1/sites/#{site_id.to_s}/visitors/#{visitor_id.to_s}.json"
-    self.class.get(path, :query => options).parsed_response
+    self.class.get(path, :query => clean(options)).parsed_response
   end
   
   def who_am_i
@@ -48,4 +34,9 @@ class Optify
     self.class.get(whois_path, :format => 'text/plain').parsed_response.chomp
   end
   
+  private
+  
+  def clean(opts)
+    opts.update(opts) { |key,val| val.is_a?(Date) ? val.strftime('%Y-%m-%d %H:%M:%S') : val.to_s }
+  end
 end
